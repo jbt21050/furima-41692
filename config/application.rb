724da68837e618e4b54e144f4f1529600e726d20
jbt21050 <jbt21050@yahoo.co.jp>
2024-11-19 19:51:56 +0900

    ユーザー認証の導入

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Furima41692
+module Item41692
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
