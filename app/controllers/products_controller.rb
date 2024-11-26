8fda3febab4678c579908f04069d24acb58073de
jbt21050 <jbt21050@yahoo.co.jp>
2024-11-26 20:46:37 +0900

    出品するボタンを押すと出品画面に遷移する

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,40 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
-    @product = Product.all
+    @products = Product.all
   end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+    @product_conditions = ProductCondition.all
+    @shipping_costs = ShippingCost.all
+    @shipping_regions = ShippingRegion.all
+    @delivery_times = DeliveryTime.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    
+    if @product.save
+      redirect_to root_path, notice: '商品が出品されました'
+    else
+      @categories = Category.all
+      @product_conditions = ProductCondition.all
+      @shipping_costs = ShippingCost.all
+      @shipping_regions = ShippingRegion.all
+      @delivery_times = DeliveryTime.all
+      render :new
+    end
+  end
+  
   private
 
-  def message_params
-    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  def product_params
+    # 必要なパラメータに画像も含める
+    params.require(:product).permit(:product_name, :product_explanation, :category_id, :product_condition_id, 
+                                    :shipping_cost_id, :shipping_region_id, :delivery_time_id, :sales_price, :image)
+          .merge(user_id: current_user.id)
   end
 end
