1ea95bf1db5863752450219f7ba65fb4b042e098
jbt21050 <jbt21050@yahoo.co.jp>
2024-12-02 13:02:49 +0900

    更新・戻るボタンで詳細ページに戻る。投稿したユーザー以外編集できない設定追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-
+  before_action :set_product, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authorize_user, only: [:edit, :update]
   def index
     @products = Product.order(created_at: :desc)
   end
@@ -13,6 +14,18 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+  
   def create
     @product = Product.create(product_params)
     @product.user = current_user
@@ -26,7 +39,15 @@ class ProductsController < ApplicationController
   
   private
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.require(:product).permit(:product_name, :product_explanation, :category_id, :product_condition_id, :shipping_cost_id, :shipping_region_id, :delivery_time_id, :sales_price, :image)
+  end
+
+  def authorize_user
+    redirect_to root_path, alert: '権限がありません。' unless current_user == @product.user
   end
 end
