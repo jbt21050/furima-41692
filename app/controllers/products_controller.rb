862c7f61d28e6cb87b0f2852542c0c6939487999
jbt21050 <jbt21050@yahoo.co.jp>
2024-12-04 17:35:02 +0900

    フォームオブジェクトを設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:edit, :update, :show, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :authorize_user, only: [:edit, :update, :destroy]
 
   def index
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
+    @product_form = ProductForm.new
   end
 
   def show
@@ -16,22 +16,38 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    
+    @product = Product.find(params[:id])
+    @product_form = ProductForm.new(
+      product_name: @product.product_name,
+      product_explanation: @product.product_explanation,
+      category_id: @product.category_id,
+      product_condition_id: @product.product_condition_id,
+      shipping_cost_id: @product.shipping_cost_id,
+      shipping_region_id: @product.shipping_region_id,
+      delivery_time_id: @product.delivery_time_id,
+      sales_price: @product.sales_price,
+      image: @product.image.attached? ? @product.image : nil,
+      product: @product
+    )
   end
 
   def update
-    if @product.update(product_params)
-      redirect_to product_path(@product)
+    @product = Product.find(params[:id])
+    @product_form = ProductForm.new(product_params.merge(product: @product))
+
+    if @product_form.update(product_params)
+      redirect_to product_path(@product), notice: '商品情報が更新されました'
     else
       render :edit, status: :unprocessable_entity
     end
   end
+
   
   def create
-    @product = Product.create(product_params)
-    @product.user = current_user
+    @product_form = ProductForm.new(product_params)
+    @product_form.user = current_user
     
-    if @product.save
+    if @product_form.save
       redirect_to root_path, notice: '商品が出品されました'
     else
       render :new, status: :unprocessable_entity
@@ -40,9 +56,9 @@ class ProductsController < ApplicationController
 
   def destroy
     if @product.destroy
-      redirect_to root_path
+      redirect_to root_path, notice: '商品を削除しました'
     else
-      redirect_to product_path(@product)
+      redirect_to product_path(@product), alert: '商品の削除に失敗しました'
     end
   end
   
@@ -53,7 +69,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:product_name, :product_explanation, :category_id, :product_condition_id, :shipping_cost_id, :shipping_region_id, :delivery_time_id, :sales_price, :image)
+    params.require(:product_form).permit(:product_name, :product_explanation, :category_id, :product_condition_id, :shipping_cost_id, :shipping_region_id, :delivery_time_id, :sales_price, :image)
   end
 
   def authorize_user
