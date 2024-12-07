f08f65d27ca29e8a05fafe75ffe666a0d0dc88a7
jbt21050 <jbt21050@yahoo.co.jp>
2024-12-07 21:27:09 +0900

    テストコード設定

--- app/models/record_form.rb
+++ app/models/record_form.rb
@@ -4,20 +4,13 @@ class RecordForm
   # 保存したいカラムをすべて列挙
   attr_accessor :user_id, :product_id, :post_code, :shipping_region_id, :municipalities,
                 :street_address, :building_name, :telephone_number, :payjp_token
-  
-  attr_accessor :shipping_information
-
-  def product
-    @product ||= Product.find(product_id)
-  end
-
 
   # バリデーション設定
   with_options presence: true do
     validates :user_id
     validates :product_id
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'は「123-4567」の形式で入力してください' }
-    validates :shipping_region_id, numericality: { other_than: 0, message: 'を選択してください' }
+    validates :shipping_region_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :municipalities
     validates :street_address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁または11桁の半角数字で入力してください' }
